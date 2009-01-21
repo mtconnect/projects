1d36b9599069a8c81f64027a7a04bb1983720de9
Will Sobel <wsobel@eecs.berkeley.edu>
2009-01-20 22:32:08 -0800

    Added cap config

---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+set :application, "projects"
+set :repository,  "ssh://deploy@projects.mtconnect.org:2233/home/deploy/projects/repo.git"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to, "/home/deploy/projects"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+set :scm, :git
+
+role :app, "projects.mtconnect.org"
+role :web, "projects.mtconnect.org"
+role :db,  "projects.mtconnect.org", :primary => true
+
+after 'deploy:setup'
+
+namespace :deploy do
+    desc "Override the default restart and execute mongel:restart task. See thin:restart"
+    task :restart, :roles => :app, :except => { :no_release => true } do
+      thin.restart
+    end
+
+    desc "Override the default start and execute thin:start"
+    task :start, :roles => :app do
+      thin.start
+    end
+
+    desc "Override the default stop and execute thin:stop"
+    task :stop, :roles => :app do
+      thin.stop
+    end
+end
